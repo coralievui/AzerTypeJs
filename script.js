644d9015975ca81d97ebd3274d077198ba05a684
Vuillemin, Coralie <coralie.vuillemin@accenture.com>
2023-10-10 15:09:28 +0200

    Update JS

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+
+let motUtilisateur = prompt("Entrez le mot : " +listeMots[0])
+
+if (motUtilisateur ===  listeMots[0]) {
+        score ++
+}
+
+motUtilisateur = prompt("Entrez le mot : " +listeMots[1])
+
+if (motUtilisateur ===  listeMots[1]) {
+        score ++
+}
+
+motUtilisateur = prompt("Entrez le mot : " +listeMots[2])
+
+if (motUtilisateur ===  listeMots[2]) {
+        score ++
+}
+
+console.log("Votre score est de " + score + " sur 3")
+
