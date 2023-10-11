0609080fef8d998cdd26541c1ffd8c40bd0118ce
Vuillemin, Coralie <coralie.vuillemin@accenture.com>
2023-10-11 09:57:00 +0200

    Final update - Test ok

---
+++ script/config.js
@@ -0,0 +1,6 @@
+// Liste de mots
+let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+
+// Liste de phrase
+let listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"]
+
---
+++ script/script.js
@@ -0,0 +1,44 @@
+function afficherResultat(score, nbMotsProposes){
+    console.log('Votre score est de ' +score+ ' sur ' +nbMotsProposes)
+}
+
+
+function choisirPhrasesOuMots(){
+    let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+
+    while (choix == ! "mots" && choix == ! "mots") {
+        choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+    }
+    return choix
+}
+
+function lancerBoucleDeJeu(listePropositions){
+    let score = 0
+    for (let i = 0; i < listePropositions.length; i++) {
+
+        let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
+
+        if (motUtilisateur === listePropositions[i]) {
+            score++
+        }
+    }
+    return score
+}
+
+function lancerLeJeu(){
+    let choix = choisirPhrasesOuMots()
+    let score = 0
+    let nbMotsProposes = 0
+
+    if(choix === "mots"){
+        score = lancerBoucleDeJeu(listeMots)
+        nbMotsProposes= listeMots.length
+    } else {
+        score = lancerBoucleDeJeu(listePhrases)
+        nbMotsProposes= listePhrases.length
+    }
+
+    afficherResultat(score,nbMotsProposes)
+}
+
+
