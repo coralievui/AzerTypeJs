824a3458c95dc48c341eddf966e56af0f95bca45
Vuillemin, Coralie <coralie.vuillemin@accenture.com>
2023-11-06 09:44:57 +0100

    Add new functions

--- script/script.js
+++ script/script.js
@@ -1,34 +1,34 @@
-function afficherResultat(score, nbMotsProposes){
+function afficherResultat(score, nbMotsProposes) {
 
     let balisezoneScoreSpan = document.querySelector(".zoneScore span")
-    
+
     let affichageScore = `${score} / ${nbMotsProposes} `
-   
+
     balisezoneScoreSpan.innerText = affichageScore
 }
 
 
-function afficherProposition(proposition){
+function afficherProposition(proposition) {
     let balisezoneProposition = document.querySelector(".zoneProposition");
 
     balisezoneProposition.innerText = proposition
 }
 
-function lancerLeJeu(){
+function lancerLeJeu() {
     let score = 0
     let i = 0
 
-    
+
     let btnValidation = document.getElementById("btnValiderMot")
     let zoneEcriture = document.getElementById("inputEcriture")
     afficherProposition(listeMots[i])
     btnValidation.addEventListener("click", () => {
-        if ( zoneEcriture.value === listeMots[i] ){
-            score ++
-            console.log=(score)
+        if (zoneEcriture.value === listeMots[i]) {
+            score++
+            console.log = (score)
         }
         i++
-        afficherResultat(score,i)
+        afficherResultat(score, i)
         zoneEcriture.value = ''
         if (listeMots[i] === undefined) {
             afficherProposition("Le jeu est fini")
@@ -39,9 +39,19 @@ function lancerLeJeu(){
 
     })
 
-    afficherResultat(score,i)
+    let listeBtnRadio = document.querySelector(".optionSource input")
+    for (let index = 0; index < listeBtnRadio.length; index++) {
+        listeBtnRadio[index].addEventListener("change", (event) => {
+            if (event.target.value === "1") {
+                listeProposition = listeMots
+            } else {
+                // Sinon nous voulons jouer avec la liste des phrases
+                listeProposition = listePhrases
+            }
+            // Et on modifie l'affichage en direct. 
+            afficherProposition(listeProposition[i])
+        })
+    }
+
+    afficherResultat(score, i)
 }
-
-
-console.log(zoneEcriture)
-
